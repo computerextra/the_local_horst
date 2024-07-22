e848ae68f478f96197af6642dc69548954aefd05
jayzone91 <register@j4yz0n3.de>
2024-07-22 11:35:03 +0200

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+
+import { api } from "@/utils/api";
+
+import { cn } from "@/lib/utils";
+import { Inter as FontSans } from "next/font/google";
+
+import "@/styles/globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <main
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Component {...pageProps} />
+      </main>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
